f18d64be9aef65b849bf2f5bf16b33763c363e72
Juan Ospina <juan.ospinal@outlook.com>
2024-03-21 21:53:25 -0500

    feat: mixin to enabled CRUD operations

--- app/db.py
+++ app/db.py
@@ -5,3 +5,37 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
+
+
+class BaseModelMixin:
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+    
+    @classmethod
+    def get_by_id(cls, id):
+        return db.session.query(cls).filter(cls.id == id).first()
+    
+
+    def create_object(data):
+        try:
+            db.session.add(data)
+            db.session.commit()
+            return data
+        except:
+            db.session.rollback()
+            raise
+
+    def update(self, data):
+        if data:
+            for key, value in data.items():
+                setattr(self, key, value)
+            db.session.commit()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            raise
